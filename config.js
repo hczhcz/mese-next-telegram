2b52f73db8e995a64cf6987452298f1c5f140e09
hcz <h.cz@qq.com>
2016-09-28 16:17:00 -0400

    add mese engine settings

--- config.js
+++ config.js
@@ -1,10 +1,23 @@
 'use strict';
 
 module.exports = {
+    // event loop
+    interval: 1000,
+
+    // gathering
     gatherTimeout: 600000,
     readyTimeout: 30000,
+
+    // engine
+    meseEngine: './mese',
+    meseMaxPlayer: 32, // TODO
+
+    // playing
     closeTimeout: 300000,
-    interval: 1000,
+    preset: 'modern',
+    settings: [{}, {}, {}, {}, {}, {}, {}, {}],
+
+    // bot
     botName: 'mese_bot',
     bot: {
         polling: true,
