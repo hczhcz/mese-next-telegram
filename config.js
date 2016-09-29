d55d281d12babcff5700c9b82c4776773b524943
hcz <h.cz@qq.com>
2016-09-29 00:31:07 -0400

    modify config

--- config.js
+++ config.js
@@ -4,16 +4,17 @@ module.exports = {
     // event loop
     interval: 1000,
 
-    // gathering
-    gatherTimeout: 600000,
-    readyTimeout: 30000,
+    // timeout
+    gatherTimeout: 600 * 1000,
+    readyTimeout: 30 * 1000,
+    closeTimeout: 30 * 1000,
+    reportTimeout: 7200 * 1000,
 
     // engine
     meseEngine: './mese',
     meseMaxPlayer: 32, // TODO
 
-    // playing
-    closeTimeout: 300000,
+    // game
     preset: 'modern',
     settings: [{}, {}, {}, {}, {}, {}, {}, {}],
 
