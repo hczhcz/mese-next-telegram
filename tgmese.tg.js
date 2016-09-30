37f5387317cf6acfe94b00f7a7d37e450d6390b6
hcz <h.cz@qq.com>
2016-09-30 15:43:12 -0400

    add submission

--- tgmese.tg.js
+++ tgmese.tg.js
@@ -65,6 +65,78 @@ const sendAll = (bot, game, i) => {
 };
 
 module.exports = (bot) => {
+    bot.onText(/([\d.]+) (\d+) ([\d.]+) ([\d.]+) ([\d.]+)$/, (msg, match) => {
+        const now = Date.now();
+
+        if (userGames[msg.from.id]) {
+            const game = userGames[msg.from.id];
+
+            const oldData = game.gameData;
+
+            core.submit(
+                game.gameData,
+                game.users[msg.from.id].index,
+                -1,
+                parseFloat(match[1]),
+                parseFloat(match[2]),
+                parseFloat(match[3]),
+                parseFloat(match[4]),
+                parseFloat(match[5]),
+                (gameData) => {
+                    if (game.gameData === oldData) {
+                        game.gameData = gameData;
+
+                        bot.sendMessage(
+                            msg.chat.id,
+                            'OK: Decision accepted\n'
+                            + '\n'
+                            + 'Pr ' + match[1] + ' '
+                            + 'Pd ' + match[2] + ' '
+                            + 'Mk ' + match[3] + ' '
+                            + 'CI ' + match[4] + ' '
+                            + 'RD ' + match[5] + '\n',
+                            {
+                                reply_to_message_id: msg.message_id,
+                            }
+                        );
+                    } else {
+                        // TODO
+                        bot.sendMessage(
+                            msg.chat.id,
+                            'Fail: System error\n'
+                            + '\n'
+                            + 'Please submit again\n',
+                            {
+                                reply_to_message_id: msg.message_id,
+                            }
+                        );
+                    }
+                },
+                (gameData) => {
+                    if (game.gameData === oldData) {
+                        game.gameData = gameData;
+                    }
+
+                    bot.sendMessage(
+                        msg.chat.id,
+                        'Fail: Decision declined\n',
+                        {
+                            reply_to_message_id: msg.message_id,
+                        }
+                    );
+                }
+            );
+        } else {
+            bot.sendMessage(
+                msg.chat.id,
+                'Fail: Game is not running now\n',
+                {
+                    reply_to_message_id: msg.message_id,
+                }
+            );
+        }
+    });
+
     bot.onTimer((now) => {
         for (const i in games) {
             const game = games[i];
