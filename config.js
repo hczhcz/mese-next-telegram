472f2b2ca65c28f2c05ae723ab8893685b018241
hcz <h.cz@qq.com>
2016-10-22 23:26:22 -0400

    fix init timeout

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     tgGatherTimeout: 600 * 1000,
     tgGatherRemind: 30 * 1000,
     tgReadyTimeout: 30 * 1000,
-    tgInitTimeout: 3 * 1000,
+    tgInitTimeout: 30 * 1000,
     tgMessage: 'Hello from MESE bot\n',
 
     meseEngine: './mese',
